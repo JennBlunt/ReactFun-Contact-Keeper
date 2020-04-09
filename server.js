d1662f17d1ba52659657f9143d77f1689ac7c619
JennBlunt <jennbluntphoto@gmail.com>
2020-04-09 14:35:32 -0400

    Set up user model, validation, password hash

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+
 const app = express();
 
 // connect database
 connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Hey! This is the ContactKeeper API!' })
 );
